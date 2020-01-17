6561509d6ad5bac7f56149bd869b0a8e0cae8bc1
zabmehta26 <41826709+zabmehta26@users.noreply.github.com>
2020-01-17 18:29:31 +0530

    Delete login.php

--- login.php
+++
@@ -1,9 +0,0 @@
-<html>
-  <body>
-    <form  action="checkin.php" method="post">
-      <input type="text" id="id" name="id" placeholder="Enter mail id" required><br>
-      <input type="password" id="password" name="password" placeholder="enter password" required><br>
-      <input type="submit" id="submit" name="submit" value="Login">
-    </form>
-  </body>
-</html>
